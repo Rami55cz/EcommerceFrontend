3a4d9f8a23805b38913cd1720d15100e13b978fc
Rami55cz <Rami55cz>
2025-04-07 15:28:30 +0100

    reconnectedFiles

--- obj/Debug/net9.0/EcommerceFrontend.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c5e8fd115fee452a818f266f5c2addb9f027ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6546528e80e1743a5df08f245aaaaf679757e154")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
