9edeece7fc21af30b1402aca0e846a4d21c7a621
Rami55cz <Rami55cz>
2025-04-07 19:00:52 +0100

    UpdatedWork

--- obj/Debug/net9.0/EcommerceFrontend.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc21b385ddc1c818a9b6a2b6fbbb82284a1b81f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8b41573ad52dd464e10b5a1345653f6fd8416a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
