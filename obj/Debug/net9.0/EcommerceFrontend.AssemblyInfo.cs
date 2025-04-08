0f85a0acd032ba485c8520a432cf8b5005efa295
Rami55cz <Rami55cz>
2025-04-08 21:04:11 +0100

    css

--- obj/Debug/net9.0/EcommerceFrontend.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edeece7fc21af30b1402aca0e846a4d21c7a621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a7d7d620ee5d2a78ff1dc307b4e929aa330a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
